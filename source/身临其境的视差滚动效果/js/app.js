e2c195f8925ef79ba3963133bb4a78f9fe320103
JWong <18810157617@163.com>
2023-10-16 12:49:01 +0800

    身临其境的视差滚动效果

---
+++ source/身临其境的视差滚动效果/js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.slider', {
+	// direction: 'vertical',
+	// freeMode: true,
+	speed: 2400,
+	parallax: true,
+	spaceBetween: 18,
+	mousewheel: {
+		enabled: true,
+		sensitivity: 2.4
+	}
+})
