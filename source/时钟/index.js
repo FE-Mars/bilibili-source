688e1aa2656652bab486ce07f25a75030fc08f77
JWong <18810157617@163.com>
2023-10-30 11:34:37 +0800

    时钟

---
+++ source/时钟/index.js
@@ -0,0 +1,35 @@
+const deg = 6;
+const hr = document.querySelector('#hr');
+const mn = document.querySelector('#mn');
+const sc = document.querySelector('#sc');
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    let ss = day.getSeconds() * deg;
+
+    hr.style.transform = `rotateZ(${(hh)+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+});
+
+const checkbox = document.getElementById("checkbox");
+const body = document.querySelector("body");
+const label = document.querySelector(".label");
+const root = document.querySelector(":root");
+const ball = document.querySelector(".ball");
+
+
+checkbox.addEventListener("change", function () {
+    if (this.checked) {
+        body.style.background = "black";
+        label.style.backgroundColor = "#40DCA5";
+        root.style.setProperty("--sec-color", "#fff");
+        ball.style.backgroundColor = "#fff";
+    } else {
+        body.style.background = "#a6bcff";
+        label.style.backgroundColor = "#fff";
+        root.style.setProperty("--sec-color", "red");
+    }
+});
