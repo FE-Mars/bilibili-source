1a442e5307735ccf1e464b040ae25c8b6f187e72
JWong <18810157617@163.com>
2024-01-08 16:29:30 +0800

    注释

--- source/Magic Mouse Effect/index.js
+++ source/Magic Mouse Effect/index.js
@@ -44,7 +44,7 @@ const createStar = position => {
   const star = document.createElement("span"),
         color = selectRandom(config.colors);
   
-  star.className = "star fa-solid fa-sparkle";
+  star.className = "star fa-solid fa-sparkle";   //字体图标class
   
   star.style.left = px(position.x);
   star.style.top = px(position.y);
